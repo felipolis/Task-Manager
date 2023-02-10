6f4699249d20948d4c667d6598853ce623d1f7f8
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-09 21:17:48 -0300

    List routes

--- api/app.js
+++ api/app.js
@@ -3,9 +3,55 @@ const app = express();
 
 const PORT = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+/* ROUTE HANDLES */
+
+
+/* LIST ROUTES */
+
+/**
+ * GET /lists
+ * Purpose: Get all lists
+ */
+app.get('/lists', (req, res) => {
+    // We want to return an array of all the lists in the database
+
 });
+
+/**
+ * POST /lists
+ * Purpose: Create a list
+ */
+app.post('/lists', (req, res) => {
+    // We want to create a new list and return the new list document back to the user (which includes the id)
+    // The list information (fields) will be passed in via the JSON request body
+
+});
+
+/**
+ * PATCH /lists/:id
+ * Purpose: Update a specified list
+ */
+app.patch('/lists/:id', (req, res) => {
+    // We want to update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+
+});
+
+/**
+ * DELETE /lists/:id
+ * Purpose: Delete a list
+ */
+app.delete('/lists/:id', (req, res) => {
+    // We want to delete the specified list (document with id in the URL)
+
+});
+
+
+
+
+
+
+
+
 
 
 
