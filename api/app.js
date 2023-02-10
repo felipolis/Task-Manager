22dd09a17ef28d6084675047d6adfe365f996d47
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-10 20:30:09 -0300

    Connecting frontend to the api

--- api/app.js
+++ api/app.js
@@ -13,6 +13,13 @@ const { List, Task } = require('./db/models');
 // Load middleware
 app.use(bodyParser.json());
 
+// CORS HEADERS MIDDLEWARE
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 /* ROUTE HANDLES */
 /* LIST ROUTES */
 
