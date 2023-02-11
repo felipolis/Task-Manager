89f14e6368e671d18d2f88fcd1fe0832a944a62f
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-11 20:28:35 -0300

    creating instace methods

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -26,3 +27,50 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+// Instance methods
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    // Return the document except the password and sessions (these shouldn't be made available)
+    return _.omit(userObject, ['password', 'sessions']);
+}
+
+userSchema.methods.generateAccessAuthToken = function () {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        // Create the JSON Web Token and return that
+        jwt.sign(
+            {
+                _id: user._id.toHexString()
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "15m"
+            },
+            (err, token) => {
+                if (!err) {
+                    resolve(token);
+                } else {
+                    // There is an error
+                    reject();
+                }
+            }
+        )
+    });
+}
+
+userSchema.methods.generateRefreshAuthToken = function () {
+    return new Promise((resolve, reject) => {
+        // Create a random 64 byte hex string
+        crypto.randomBytes(64, (err, buf) => {
+            if (!err) {
+                // No error
+                let token = buf.toString('hex');
+
+                return resolve(token);
+            }
+        })
+    });
+}
